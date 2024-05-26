b5c45200ccf5937ab3f63734b2ad5b3bf8bcd445
Jerry Ji <jerryji@Jerrys-MacBook-Air.local>
2024-05-26 23:41:30 +0800

    website init with sidebar initial thought layout lol

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "./layouts/sidebar";
+
+function App() {
+  return (
+    <>
+      <Sidebar>
+        <h1>Hi, I am Jerry. Cornell CS + Math</h1>
+        <p>This is the main content area.</p>
+      </Sidebar>
+    </>
+  );
+}
+
+export default App;
