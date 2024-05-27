e43545122962ea4b525419500812850d76c69a17
gerald <jji.bigg@gmail.com>
2024-05-28 00:38:39 +0800

    skills dumping

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -8,14 +8,17 @@ const Skills = () => {
 
   return (
     <Sidebar current={location.pathname}>
+      <h1 className="text-2xl font-semibold text-gray-900">
+        Tech Stacks I've worked with
+      </h1>
       <LogoSkills
         logos={[
-          "https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg",
-          "https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg",
-          "https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg",
-          "https://tailwindui.com/img/logos/158x48/laravel-logo-gray-900.svg",
-          "https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg",
-          "https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg",
+          "/images/logos/mongodb.png",
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/MySQL_textlogo.svg/2560px-MySQL_textlogo.svg.png",
+          "https://akztcujgjipefxjrfsak.supabase.co/storage/v1/object/public/statics/perks/light/supabase.png",
+          "/images/logos/pyt.png",
+          "https://rust-lang.org/logos/rust-logo-512x512.png",
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/2560px-Nextjs-logo.svg.png",
         ]}
       />
     </Sidebar>
