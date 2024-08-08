6708256e52532a09cb7c8693872c9209cfdf0edb
Jerry Ji <104660649+jji-bigG@users.noreply.github.com>
2024-08-09 03:54:20 +0800

    Update sidebar.jsx

--- src/layouts/sidebar.jsx
+++ src/layouts/sidebar.jsx
@@ -312,7 +312,7 @@ const Sidebar =
                           </li>
                           <li className="mt-auto">
                             <a
-                              href="#"
+                              href="/Ruiyang Ji Resume.pdf"
                               className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-sky-200 hover:bg-sky-700 hover:text-white"
                             >
                               <ClipboardDocumentCheckIcon
